0c8bc5ab0e9ceecc5a5e5fb2a963ef06cadbb79a
Hoff Inc <hoff.rj@gmail.com>
2023-02-04 05:53:51 +0000

    include Content.mgcb

--- GGJ-Ideas-and-Monogame-trials/Game1.cs
+++ GGJ-Ideas-and-Monogame-trials/Game1.cs
@@ -1,6 +1,12 @@
-﻿using Microsoft.Xna.Framework;
+using System.Diagnostics;
+using System.Reflection;
+using System.Threading;
+using System.Windows.Forms.Design;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+
 
 namespace GGJ_Ideas_and_Monogame_trials
 {
@@ -14,39 +20,96 @@ namespace GGJ_Ideas_and_Monogame_trials
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
+            Window.AllowUserResizing = true;
         }
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+            // Vector3 v3Position = new Vector3(0, 0, 0);
+            // Matrix.CreateTranslation(v3Position);
+
+
+
+
 
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
+            // Content = new ContentManager(this.Services, "Content");
+
+            // _spriteBatch; needed to draw textures?
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
+
+            // Model model = Content.Load<Model>("SimpleShip\\Ship.fbx");
+
         }
 
+
+
+
+
+
+        /*
+         *
+         * Notes
+         * -----
+         * Called on each game-loop
+         * Target fps (default) = 60
+         *
+         * gameTime.TotalGameTime = global clock
+         * gameTime.ElapsedTime, time since last update (= 1/60 seconds)
+         *
+         *
+         *
+         */
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            {
                 Exit();
+            }
 
-            // TODO: Add your update logic here
 
             base.Update(gameTime);
         }
 
+
+        /*
+         * Notes
+         * -----
+         * Same as Update. Called immediately after on the same thread
+         *
+         * attaching an object of type
+         *
+         *      DrawableGameComponent
+         *
+         *
+         * will have its Draw(GameTime gametime) method implicitly called at this step (needs to be registered)
+         *
+         *
+         *
+         *
+         *
+         */
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
+
 
             base.Draw(gameTime);
+        }
+
+
+
+        // 'using' statement in Program.cs ensures call at application close
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+            // Debug.WriteLine($"disposed was called");
         }
     }
 }
--- GGJ-Ideas-and-Monogame-trials/Program.cs
+++ GGJ-Ideas-and-Monogame-trials/Program.cs
@@ -1,3 +1,23 @@
-﻿
-using var game = new GGJ_Ideas_and_Monogame_trials.Game1();
-game.Run();
+using System;
+using System.Diagnostics;
+using Microsoft.Xna.Framework;
+
+namespace GGJ_Ideas_and_Monogame_trials
+{
+    public static class Program
+    {
+        [STAThread]
+        public static void Main()
+        {
+            using Game1 game = new();
+            game.Run();
+
+
+        }
+    }
+}
+
+
+
+
+
