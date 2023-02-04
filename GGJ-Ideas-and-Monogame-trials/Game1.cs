ef1c440e2c177227b952c14a727875147c50b19f
Hoff Inc <hoff.rj@gmail.com>
2023-02-04 10:30:31 +0000

    rendering using DrawIndexedPrimitives but can't find the geometry

--- GGJ-Ideas-and-Monogame-trials/Game1.cs
+++ GGJ-Ideas-and-Monogame-trials/Game1.cs
@@ -1,5 +1,5 @@
 using System;
-using System.Diagnostics;
+using System.Runtime.CompilerServices;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
@@ -10,6 +10,10 @@ using Microsoft.Xna.Framework.Input;
  * <remarks>constructor</remarks>
  * - `new GraphicsDeviceManager(this)` attaches itself to this.GraphicsDevice
  * - removed 'spritebatch' (refer to template)
+ *
+ * <remarks>Initialize()</remarks>
+ * - called once after constructor
+ * - GraphicsDevice will be ready
  *
  * <remarks>method Update(GameTime gameTime)</remarks>
  * Called on each game-loop
@@ -36,6 +40,9 @@ namespace GGJ_Ideas_and_Monogame_trials
 
         private int DEFAULT_VIEWPORT_WIDTH = 800;
         private int DEFAULT_VIEWPORT_HEIGHT = 600;
+        private float FOV = MathF.PI / 4;
+        private float NEAR_CLIP = 0.1f;
+        private float FAR_CLIP = 100f;
 
 
         public Game1()
@@ -63,8 +70,38 @@ namespace GGJ_Ideas_and_Monogame_trials
             return (float) Window.ClientBounds.Width / Window.ClientBounds.Height;
         }
 
+        private float Deg(float degrees)
+        {
+            return degrees * MathF.PI / 180;
+        }
+
+
+
+
+        private BasicEffect basicEffect;
+
         protected override void Initialize()
         {
+            // enable these lines if using SpriteBatch
+            GraphicsDevice.BlendState = BlendState.Opaque;
+            GraphicsDevice.DepthStencilState = DepthStencilState.Default;
+            GraphicsDevice.SamplerStates[0] = SamplerState.LinearWrap;
+
+            // render both sides of polygons
+            GraphicsDevice.RasterizerState = RasterizerState.CullNone;
+
+            // -- where is BasicEffect?
+            // BasicEffect.World = Matrix.CreateTranslation(new Vector3(0, 0, 0));
+            basicEffect = new BasicEffect(GraphicsDevice);
+            basicEffect.AmbientLightColor = Vector3.One;
+            basicEffect.DirectionalLight0.Enabled = true;
+            basicEffect.DirectionalLight0.DiffuseColor = Vector3.One;
+            basicEffect.DirectionalLight0.Direction = Vector3.Normalize(Vector3.One);
+            basicEffect.World = world;
+            basicEffect.View = view;
+            basicEffect.Projection = projection;
+            // use my own vertex colors
+            basicEffect.VertexColorEnabled = true;
             base.Initialize();
         }
 
@@ -81,16 +118,63 @@ namespace GGJ_Ideas_and_Monogame_trials
             {
                 Exit();
             }
-            world = Matrix.CreateRotationY((float) gameTime.TotalGameTime.TotalSeconds);
-            projection = Matrix.CreatePerspectiveFieldOfView(MathF.PI / 4, ViewportAspectRatio(), 0.1f, 100f);
+
+            if (Mouse.GetState().LeftButton == ButtonState.Pressed)
+            {
+                // Debug.WriteLine($"mouse down");
+            }
+
+            // world = Matrix.CreateRotationY((float) gameTime.TotalGameTime.TotalSeconds);
+            world = Matrix.CreateRotationY(Deg(30));
+            projection = Matrix.CreatePerspectiveFieldOfView(FOV, ViewportAspectRatio(), NEAR_CLIP, FAR_CLIP);
             base.Update(gameTime);
         }
 
+
         protected override void Draw(GameTime gameTime)
         {
+            // GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Black);
+            // DrawModel(model, world, view, projection);
 
-            GraphicsDevice.Clear(Color.CornflowerBlue);
-            DrawModel(model, world, view, projection);
+
+
+            basicEffect.CurrentTechnique.Passes[0].Apply();
+
+
+            float left = 0f;
+            float bottom = 0f;
+            float right = 10f;
+            float top = 10f;
+            VertexPositionColor[] primitiveList = new VertexPositionColor[4];
+            primitiveList[0] = new VertexPositionColor(new Vector3(left, 0, bottom), Color.Green);
+            primitiveList[1] = new VertexPositionColor(new Vector3(left, 0, top), Color.Green);
+            primitiveList[2] = new VertexPositionColor(new Vector3(right, 0, top), Color.Green);
+
+            short[] lineListIndices = new short[3];
+            lineListIndices[0] = 0;
+            lineListIndices[1] = 1;
+            lineListIndices[2] = 2;
+
+            // draw a vertex-buffer
+            VertexBuffer vertexBuffer = new VertexBuffer(GraphicsDevice, typeof(VertexPositionColor), 4, BufferUsage.None);
+            vertexBuffer.SetData<VertexPositionColor>(primitiveList);
+            IndexBuffer lineListIndexBuffer = new IndexBuffer(
+                GraphicsDevice,
+                IndexElementSize.SixteenBits,
+                sizeof(short) * lineListIndices.Length,
+                BufferUsage.None);
+            lineListIndexBuffer.SetData<short>(lineListIndices);
+            GraphicsDevice.Indices = lineListIndexBuffer;
+            GraphicsDevice.SetVertexBuffer(vertexBuffer);
+            // GraphicsDevice.DrawIndexedPrimitives(PrimitiveType.LineList, 0, 0, 8, 0, 7);
+            // GraphicsDevice.DrawIndexedPrimitives(PrimitiveType.LineList, 0, 0, 1, 0, 1);
+            GraphicsDevice.DrawIndexedPrimitives(PrimitiveType.LineList, 0, 4, 3);
+
+
+
+
+
             base.Draw(gameTime);
         }
 
---
+++ GGJ-Ideas-and-Monogame-trials/Geometries/GroundPlane.cs
@@ -0,0 +1,38 @@
+
+
+using Microsoft.Xna.Framework.Content.Pipeline.Graphics;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GGJ_Ideas.Geometries
+{
+    public class GroundPlane
+    {
+        private static float left = 0f;
+        private static float bottom = 0f;
+        private static float right = 10f;
+        private static float top = 10f;
+
+        // public Model(GraphicsDevice graphicsDevice, List<ModelBone> bones, List<ModelMesh> meshes)
+        public static ModelMesh CreateTestTriangle()
+        {
+            MeshBuilder builder = MeshBuilder.StartMesh("TestTriangle");
+            builder.CreatePosition(left, 0, bottom);     // v0
+            builder.CreatePosition(left, 0, top);        // v1
+            builder.CreatePosition(right, 0, top);       // v2
+            builder.CreatePosition(right, 0, bottom);    // v3
+            builder.AddTriangleVertex(0);
+            builder.AddTriangleVertex(1);
+            builder.AddTriangleVertex(2);
+            builder.FinishMesh(); // returns MeshContent
+
+            return null;
+        }
+    }
+}
+
+
+
+
+
+
+
