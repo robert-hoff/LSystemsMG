130bf69249921d1c211aae7a5f0a32143d069f8c
Hoff Inc <hoff.rj@gmail.com>
2023-02-04 06:24:12 +0000

    rotating mesh (spaceship)

--- GGJ-Ideas-and-Monogame-trials/Game1.cs
+++ GGJ-Ideas-and-Monogame-trials/Game1.cs
@@ -1,7 +1,4 @@
-using System.Diagnostics;
-using System.Reflection;
-using System.Threading;
-using System.Windows.Forms.Design;
+using System;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
@@ -12,12 +9,19 @@ namespace GGJ_Ideas_and_Monogame_trials
 {
     public class Game1 : Game
     {
-        private GraphicsDeviceManager _graphics;
-        private SpriteBatch _spriteBatch;
+        // GraphicsDeviceManager graphics;
+        // SpriteBatch spriteBatch;
+
+        private Model model;
+        private Matrix world = Matrix.CreateTranslation(new Vector3(0, 0, 0));
+        private Matrix view = Matrix.CreateLookAt(new Vector3(0, 0, 5), new Vector3(0, 0, 0), Vector3.UnitY);
+        private Matrix projection = Matrix.CreatePerspectiveFieldOfView(MathF.PI / 4, 1.6f, 0.1f, 100f);
+
 
         public Game1()
         {
-            _graphics = new GraphicsDeviceManager(this);
+            // R: this doesn't need to be assigned
+            _ = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
             Window.AllowUserResizing = true;
@@ -25,30 +29,17 @@ namespace GGJ_Ideas_and_Monogame_trials
 
         protected override void Initialize()
         {
-            // Vector3 v3Position = new Vector3(0, 0, 0);
-            // Matrix.CreateTranslation(v3Position);
-
-
-
-
-
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
-            // Content = new ContentManager(this.Services, "Content");
+            // spriteBatch - needed to draw textures?
+            _ = new SpriteBatch(GraphicsDevice);
 
-            // _spriteBatch; needed to draw textures?
-            _spriteBatch = new SpriteBatch(GraphicsDevice);
-
-
-            // Model model = Content.Load<Model>("SimpleShip\\Ship.fbx");
-
+            Content = new ContentManager(this.Services, "Content");
+            model = Content.Load<Model>("ship-no-texture");
         }
-
-
-
 
 
 
@@ -72,7 +63,7 @@ namespace GGJ_Ideas_and_Monogame_trials
                 Exit();
             }
 
-
+            world = Matrix.CreateRotationY((float) gameTime.TotalGameTime.TotalSeconds);
             base.Update(gameTime);
         }
 
@@ -96,20 +87,35 @@ namespace GGJ_Ideas_and_Monogame_trials
          */
         protected override void Draw(GameTime gameTime)
         {
+            // GraphicsDevice.Clear(Color.Black);
             GraphicsDevice.Clear(Color.CornflowerBlue);
-
-
-
+            // GraphicsDevice and graphics.GraphicsDevice is the same method
+            // graphics.GraphicsDevice.Clear(Color.CornflowerBlue);
+            DrawModel(model, world, view, projection);
             base.Draw(gameTime);
         }
 
+        private void DrawModel(Model model, Matrix world, Matrix view, Matrix projection)
+        {
+            foreach (ModelMesh mesh in model.Meshes)
+            {
+                foreach (BasicEffect effect in mesh.Effects)
+                {
+                    effect.World = world;
+                    effect.View = view;
+                    effect.Projection = projection;
+                }
+                mesh.Draw();
+            }
+        }
 
 
-        // 'using' statement in Program.cs ensures call at application close
+        // 'using' statement in Program.cs ensures call to Dispose(..) at application close
         protected override void Dispose(bool disposing)
         {
             base.Dispose(disposing);
             // Debug.WriteLine($"disposed was called");
         }
+
     }
 }
